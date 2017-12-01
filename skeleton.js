805d7d81fe343864ccf855438706ea424125d2fe
Spencer Guest <spencer.b.guest@gmail.com>
2017-11-30 23:12:41 -0600

    Clean whitespace in skeleton even if not parsing by line

--- skeleton.js
+++ skeleton.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 fs.readFile(__dirname + '\\input.txt', 'utf-8', (err, data) => {
-    for(var line of data.trim().split('\n')) {
+    data = data.trim();
+    for(var line of data.split('\n')) {
     }
 });
