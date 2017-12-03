7ec1df931e90025a15367aa143b0ce504a553f8a
Spencer Guest <spencer.b.guest@gmail.com>
2017-12-02 23:23:24 -0600

    2017-03 raw

---
+++ 2017/03/part1.js
@@ -0,0 +1,49 @@
+let input = 368078;
+
+let x = 0;
+let y = 0;
+
+let maxX = 0;
+let maxY = 0;
+let minX = 0;
+let minY = 0;
+
+let direction = 'R';
+
+let index = 1;
+
+while(index < input) {
+    if(direction === 'R') {
+        x += 1;
+        if(x > maxX) {
+            maxX = x;
+            direction = 'U';
+        }
+    }
+    else if(direction === 'L') {
+        x -= 1;
+        if(x < minX) {
+            minX = x;
+            direction = 'D';
+        }
+    }
+    else if(direction === 'U')
+    {
+        y -= 1;
+        if(y < minY) {
+            minY = y;
+            direction = 'L';
+        }
+    }
+    else if(direction === 'D') {
+        y += 1;
+        if(y > maxY) {
+            maxY = y;
+            direction = 'R';
+        }
+    }
+    index++;
+}
+
+console.log(x, y);
+console.log(Math.abs(x) + Math.abs(y));
