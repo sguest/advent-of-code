260371e674177bff5f8d0f7ea64da8d97b31db19
Spencer Guest <spencer.b.guest@gmail.com>
2017-12-03 00:38:43 -0600

    2015-10 raw

---
+++ 2015/10/part2.js
@@ -0,0 +1,26 @@
+let input = '3113322113';
+
+for(let count = 0; count < 50; count++) {
+    let newVal = '';
+
+    let current = '';
+    let currentCount = 0;
+
+    for(let char of input) {
+        if(char === current) {
+            currentCount++;
+        }
+        else {
+            if(currentCount > 0) {
+                newVal += currentCount + current;
+            }
+            currentCount = 1;
+            current = char;
+        }
+    }
+    newVal += currentCount + current;
+
+    input = newVal;
+}
+
+console.log(input.length);
