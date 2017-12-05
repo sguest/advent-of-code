a5da1f1623c06bb59d57f04deee50456b29fb0d7
Spencer Guest <spencer.b.guest@gmail.com>
2017-12-04 23:10:26 -0600

    2017-05 raw

---
+++ 2017/05/part2.js
@@ -0,0 +1,27 @@
+let lib = require('../../lib');
+
+let year = 2017;
+let day = 05;
+
+lib.getInput(year, day).then((data) => {
+    let jumps = [];
+    for(var line of data.split('\n')) {
+        jumps.push(parseInt(line, 10));
+    }
+
+    let index = 0;
+    let steps = 0;
+
+    while(index >= 0 && index < jumps.length) {
+        let jumpNum = jumps[index];
+        if(jumpNum >= 3) {
+            jumps[index]--;            
+        }
+        else {
+            jumps[index]++;            
+        }
+        index += jumpNum;
+        steps++;
+    }
+    console.log(steps);
+});
