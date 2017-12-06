28487daa466ea1356992dc942a3caf5fc37a834f
Spencer Guest <spencer.b.guest@gmail.com>
2017-12-05 22:31:36 -0600

    2015-13 raw

---
+++ 2015/13/part1.js
@@ -0,0 +1,43 @@
+let lib = require('../../lib');
+
+let year = 2015;
+let day = 13;
+
+lib.getInput(year, day).then((data) => {
+    let parser = /^([a-z]+) would (gain|lose) (\d+) happiness units by sitting next to ([a-z]+)\.$/i;
+    let people = [];
+    let deltas = {};
+    for(var line of data.split('\n')) {
+        let parsed = parser.exec(line);
+
+        if(people.indexOf(parsed[1]) === -1) {
+            people.push(parsed[1]);
+        }
+
+        if(people.indexOf(parsed[4]) === -1) {
+            people.push(parsed[4]);
+        }
+
+        let amount = parseInt(parsed[3]);
+
+        if(parsed[2] === 'lose') {
+            amount = -amount;
+        }
+
+        deltas[parsed[1] + parsed[4]] = amount;
+    }      
+
+    let maxHappiness = 0;
+
+    for(let order of lib.permutations(people)) {
+        let happiness = 0;
+
+        for(let index = 0; index < order.length; index++) {
+            happiness += deltas[order[index] + order[(index + 1) % order.length]] + deltas[order[(index + 1) % order.length] + order[index]];
+        }
+
+        maxHappiness = Math.max(happiness, maxHappiness);
+    }
+
+    console.log(maxHappiness);
+});
