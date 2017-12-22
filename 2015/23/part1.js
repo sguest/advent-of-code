72188de572430216cf60f7403b7ab07d7dff1dea
Spencer Guest <spencer.b.guest@gmail.com>
2017-12-21 18:13:35 -0600

    2015-23 raw

---
+++ 2015/23/part1.js
@@ -0,0 +1,55 @@
+let lib = require('../../lib');
+
+let year = 2015;
+let day = 23;
+
+lib.getInput(year, day).then((data) => {
+    let ops = [];
+
+    for(let line of data.split('\n')) {
+        ops.push(line.split(/,? /));
+    }
+
+    let pointer = 0;
+    let registers = { a: 0, b: 0 };
+
+    while(pointer >= 0 && pointer < ops.length) {
+        let current = ops[pointer];
+        let instruction = current[0];
+        if(instruction === 'hlf') {
+            registers[current[1]] /= 2;
+            pointer++;
+        }
+        else if(instruction === 'tpl') {
+            registers[current[1]] *= 3;
+            pointer++;
+        }
+        else if(instruction === 'inc') {
+            registers[current[1]]++;
+            pointer++;
+        }
+        else if(instruction === 'jmp') {
+            pointer += parseInt(current[1], 10);
+        }
+        else if(instruction === 'jie') {
+            if(registers[current[1]] % 2 === 0) {
+                pointer += parseInt(current[2], 10);
+            }
+            else {
+                pointer++;
+            }
+        }
+        else if(instruction === 'jio') {
+            if(registers[current[1]] === 1) {
+                pointer += parseInt(current[2], 10);
+            }
+            else {
+                pointer++;
+            }
+        }
+    }
+
+    console.log(registers.b);
+}).catch((err) => {
+    console.log(err.stack);
+});
