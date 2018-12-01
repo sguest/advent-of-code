35845d495c0ca95ce80c23d50c5eef2d6bd4f5bd
Spencer Guest <spencer.b.guest@gmail.com>
2018-11-30 22:52:33 -0600

    Root package.json, init script

--- skeleton.js
+++ skeleton.js
@@ -1,7 +1,7 @@
 let lib = require('../../lib');
 
-let year = ;
-let day = ;
+let year = {{year}};
+let day = {{day}};
 
 lib.getInput(year, day).then((data) => {
     for(let line of data.split('\n')) {
