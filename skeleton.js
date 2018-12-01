d0e20162dd4ae3c59b206b47851e95e8f407e5bb
Spencer Guest <spencer.b.guest@gmail.com>
2018-11-30 23:24:17 -0600

    Skeleton whitespace

--- skeleton.js
+++ skeleton.js
@@ -6,7 +6,7 @@ let day = {{day}};
 lib.getInput(year, day).then((data) => {
     for(let line of data.split('\n')) {
         
-    }      
+    }
 }).catch((err) => {
     console.log(err.stack);
 });
