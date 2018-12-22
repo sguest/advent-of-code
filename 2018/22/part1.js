0ef301b661b79c72aaeb1ca2b54c29336c1cab54
Spencer Guest <spencer.b.guest@gmail.com>
2018-12-22 00:06:01 -0600

    2018-22

---
+++ 2018/22/part1.js
@@ -0,0 +1,44 @@
+let lib = require('../../lib');
+
+let year = 2018;
+let day = 22;
+
+lib.getInput(year, day).then((data) => {
+    let depth = 11739;
+    let targetX = 11;
+    let targetY = 718;
+
+    let erosion = [];
+
+    let total = 0;
+
+    for(let x = 0; x <= targetX; x++) {
+        for(let y = 0; y <= targetY; y++) {
+            let geoIndex;
+
+            if((x === 0 && y === 0) || (x === targetX && y === targetY)) {
+                geoIndex = 0;
+            }
+            else if(x === 0) {
+                geoIndex = y * 48271;
+            }
+            else if(y === 0) {
+                geoIndex = x * 16807;
+            }
+            else {
+                geoIndex = erosion[x-1][y] * erosion[x][y-1];
+            }
+
+            let erosionVal = (geoIndex + depth) % 20183;
+
+            erosion[x] = erosion[x] || [];
+            erosion[x][y] = erosionVal;
+
+            total += erosionVal % 3;
+        }
+    }
+    
+    console.log(total);
+}).catch((err) => {
+    console.log(err.stack);
+});
