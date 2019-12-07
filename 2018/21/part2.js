f44361e5db061cbb05346586c403b5adf2a10d97
sguest <spencer.b.guest@gmail.com>
2019-12-07 17:10:02 -0600

    Use rewritten version of 2018-21 part 2 for speed, it's a reverse engineering problem anyways

--- 2018/21/part2.js
+++ 2018/21/part2.js
@@ -3,65 +3,34 @@ let lib = require('../../lib');
 let year = 2018;
 let day = 21;
 
-let ops = {
-    addr: (reg, a, b) => reg[a] + reg[b],
-    addi: (reg, a, b) => reg[a] + b,
-    mulr: (reg, a, b) => reg[a] * reg[b],
-    muli: (reg, a, b) => reg[a] * b,
-    banr: (reg, a, b) => reg[a] & reg[b],
-    bani: (reg, a, b) => reg[a] & b,
-    borr: (reg, a, b) => reg[a] | reg[b],
-    bori: (reg, a, b) => reg[a] | b,
-    setr: (reg, a, b) => reg[a],
-    seti: (reg, a, b) => a,
-    gtir: (reg, a, b) => a > reg[b] ? 1 : 0,
-    gtri: (reg, a, b) => reg[a] > b ? 1 : 0,
-    gtrr: (reg, a, b) => reg[a] >  reg[b] ? 1 : 0,
-    eqir: (reg, a, b) => a === reg[b] ? 1 : 0,
-    eqri: (reg, a ,b) => reg[a] === b ? 1 : 0,
-    eqrr: (reg, a, b) => reg[a] === reg[b] ? 1 : 0
-}
-
 lib.getInput(year, day).then((data) => {
     let lines = data.split('\n');
 
-    let ipLine = lines.shift();
-    let ip = +(ipLine.split(' ')[1]);
+    let const1 = +(lines[8].split(' ')[1]);
+    let const2 = +(lines[12].split(' ')[2]);
 
-    let instructions = [];
-    for(let line of lines) {
-        instructions.push(line.split(' ').map(x => {
-            if(isNaN(+x)) {
-                return x;
-            }
-            return +x;
-        }));
-    }
-
-    let registers = [0, 0, 0, 0, 0, 0];
-
-    let found = {};
     let last = 0;
+    let found = {};
 
-    while(registers[ip] >= 0 && registers[ip] < instructions.length)
-    {
-        if(registers[ip] === 28) {
-            if(found[registers[4]]) {
-                console.log(last);
-                break;
-            }
-            else {
-                found[registers[4]] = true;
-                last = registers[4];
-            }
+    let c, d, e = 0;
+
+    while(true) {
+        if(found[e]) {
+            console.log(last);
+            break;
         }
-        let current = instructions[registers[ip]];
+        else {
+            found[e] = true;
+            last = e;
+        }
 
-        let result = ops[current[0]](registers, current[1], current[2]);
-
-        registers[current[3]] = result;
-
-        registers[ip]++;
+        d = e | 0x10000;
+        e = const1;
+        while(d > 0) {
+            c = d & 0xff;
+            e = ((e + c) & 0xffffff) * const2 & 0xffffff;
+            d = d >> 8;
+        }
     }
 }).catch((err) => {
     console.log(err.stack);
