62a87159f2cea4ab17e75b02f0a5f8d6bc7057cc
sguest <spencer.b.guest@gmail.com>
2022-06-29 20:30:51 -0500

    Make 2018-23 more generic
    Closes #1

--- 2018/23/part2.js
+++ 2018/23/part2.js
@@ -1,10 +1,10 @@
 let lib = require('../../lib');
+let { PriorityQueue } = require('@datastructures-js/priority-queue');
 
 let year = 2018;
 let day = 23;
 
-// shamelessly stolen and adapted from https://www.reddit.com/r/adventofcode/comments/a8s17l/2018_day_23_solutions/ecerldd/
-
+// Adapted from https://www.reddit.com/r/adventofcode/comments/a8s17l/2018_day_23_solutions/ecfmpy0/?utm_source=reddit&utm_medium=web2x&context=3
 lib.getInput(year, day).then((data) => {
     let lines = data.split('\n');
     let bots = [];
@@ -20,50 +20,84 @@ lib.getInput(year, day).then((data) => {
     let maxY = Math.max(...bots.map(b => b.y), 0);
     let minZ = Math.min(...bots.map(b => b.z), 0);
     let maxZ = Math.max(...bots.map(b => b.z), 0);
+    let maxCoordinate = Math.max(...[minX, maxX, minY, maxY, minZ, maxZ].map(Math.abs));
 
     let searchDistance = 1;
-    while(searchDistance < maxX - minX) {
+    while(searchDistance < maxCoordinate) {
         searchDistance *= 2;
     }
 
-    while(true) {
-        let bestCount = 0;
-        let bestLocation;
-        let bestDistance = Infinity;
+    let queue = new PriorityQueue((a, b) => b.numBots - a.numBots || b.searchSize - a.searchSize || a.distance - b.distance);
+    queue.enqueue({
+        numBots: bots.length,
+        searchSize: 2 * searchDistance,
+        distance: 3 * searchDistance,
+        minX: -searchDistance,
+        minY: -searchDistance,
+        minZ: -searchDistance,
+    });
 
-        for(let x = minX; x <= maxX + 1; x += searchDistance) {
-            for(let y = minY; y <= maxY + 1; y += searchDistance) {
-                for(let z = minZ; z <= maxZ + 1; z += searchDistance) {
-                    let count = 0;
-                    for(let bot of bots) {
-                        let botDistance = Math.abs(x - bot.x) + Math.abs(y - bot.y) + Math.abs(z - bot.z)
-                        if(botDistance - bot.range < searchDistance) {
-                            count++;
-                        }
-                    }
+    let deltas = [];
 
-                    let currentVal = Math.abs(x) + Math.abs(y) + Math.abs(z);
-                    if(count > bestCount || (count === bestCount && currentVal < bestDistance)) {
-                        bestCount = count;
-                        bestDistance = currentVal;
-                        bestLocation = { x, y, z };
-                    }
-                }
+    for(let x of [0, 1]) {
+        for(let y of [0, 1]) {
+            for(let z of [0, 1]) {
+                deltas.push({x, y, z});
             }
         }
+    }
 
-        if(searchDistance === 1) {
-            console.log(bestDistance);
+    while(!queue.isEmpty()) {
+        let current = queue.dequeue();
+
+        if(current.searchSize === 1) {
+            console.log(current.distance);
             break;
         }
-        else {
-            minX = bestLocation.x - searchDistance;
-            maxX = bestLocation.x + searchDistance;
-            minY = bestLocation.y - searchDistance;
-            maxY = bestLocation.y + searchDistance;
-            minZ = bestLocation.z - searchDistance;
-            maxZ = bestLocation.z + searchDistance;
-            searchDistance /= 2;
+
+        let newSize = current.searchSize / 2;
+
+        for(let delta of deltas) {
+            let minX = current.minX + newSize * delta.x;
+            let minY = current.minY + newSize * delta.y;
+            let minZ = current.minZ + newSize * delta.z;
+            let maxX = minX + newSize - 1;
+            let maxY = minY + newSize - 1;
+            let maxZ = minZ + newSize - 1;
+
+            let numBots = bots.filter(bot => {
+                let distance = 0;
+                if(bot.x < minX) {
+                    distance += minX - bot.x;
+                }
+                else if(bot.x > maxX) {
+                    distance += bot.x - maxX;
+                }
+                if(bot.y < minY) {
+                    distance += minY - bot.y;
+                }
+                else if(bot.y > maxY) {
+                    distance += bot.y - maxY;
+                }
+                if(bot.z < minZ) {
+                    distance += minZ - bot.z;
+                }
+                else if(bot.z > maxZ) {
+                    distance += bot.z - maxZ;
+                }
+                return (distance <= bot.range);
+            }).length;
+
+            let distance = Math.abs(minX) + Math.abs(minY) + Math.abs(minZ);
+
+            queue.enqueue({
+                numBots,
+                searchSize: newSize,
+                distance,
+                minX,
+                minY,
+                minZ,
+            })
         }
     }
 }).catch((err) => {
