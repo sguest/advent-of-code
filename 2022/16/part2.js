70ce71609b6ebff7e8d205f137fb03c8dbd38e1f
sguest <spencer.b.guest@gmail.com>
2023-02-12 23:00:15 -0600

    2022-16 finally

--- 2022/16/part2.js
+++ 2022/16/part2.js
@@ -1,12 +1,54 @@
 let lib = require('../../lib');
+let Combinatorics = require('js-combinatorics');
 
 let year = 2022;
 let day = 16;
 
+/*
+    This solution uses a technique found here
+    https://www.reddit.com/r/adventofcode/comments/zn6k1l/comment/j1xsxd2/?utm_source=share&utm_medium=web2x&context=3
+    The technique splits the input into 2 equal size lists and solves each one independently, and then runs each pair
+    of possible lists together, and takes the largest total size. I'm not convinced this works for every possible case
+    (for example it seems to slightly under-state the sample input) but it gives the correct answer and it's good enough
+    for me, for now at least
+ */
+
+let valves = { AA: { rate: 0, distances: {} } };
+
+function findBest(targets) {
+    let start = { at: 'AA', time: 27, released: 0, opened: [] };
+    let best = 0;
+    let bestAt = {};
+
+    let queue = new lib.linkedList();
+    queue.push(start);
+
+    while(queue.any()) {
+        let current = queue.shift();
+        let newOpened = [...current.opened];
+        newOpened.push(current.at);
+        newOpened.sort();
+        let identifier = newOpened.join(',');
+        let valve = valves[current.at];
+        let released = current.released + valve.rate * (current.time - 1);
+        if(current.time > 0 && (!bestAt[identifier] || bestAt[identifier] < released)) {
+            bestAt[identifier] = released;
+            best = Math.max(best, released);
+
+            for(let target of targets) {
+                if(!current.opened.includes(target)) {
+                    queue.push({ at: target, time: current.time - valve.distances[target] - 1, released, opened: newOpened });
+                }
+            }
+        }
+    }
+
+    return best;
+}
+
 lib.getInput(year, day).then((data) => {
     let lines = data.split('\n');
     let paths = {};
-    let valves = { AA: { rate: 0, distances: {} } };
     for(let line of lines) {
         let parsed = /^Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.+)$/.exec(line);
         let name = parsed[1];
@@ -51,65 +93,31 @@ lib.getInput(year, day).then((data) => {
         }
     }
 
-    let start = { at: ['AA', 'AA'], distance: [0, 0], time: 26, released: 0, visited: [[], []], history: [[], []] };
-    let queue = new lib.linkedList();
-    queue.push(start);
+    let len1 = Math.floor(targets.length / 2);
+    let len2 = targets.length - len1;
+
+    let comb1 = Combinatorics.combination(targets, len1);
+    let list1 = [];
+    let x;
+    while(x = comb1.next()) {
+        list1.push(x);
+    }
+    let comb2 = Combinatorics.combination(targets, len2);
+    let list2 = [];
+    while(x = comb2.next()) {
+        list2.push(x);
+    }
+
     let best = 0;
 
-    let bestByVisited = {};
-
-    while(queue.any()) {
-        let current = queue.shift();
-
-        if(current.time > 0) {
-            if(current.distance[0] > 0 && current.distance[1] > 0) {
-                let minDistance = Math.min(...current.distance);
-                queue.push({ ...current, distance: current.distance.map(d => d - minDistance), time: current.time - minDistance });
+    for(let path1 of list1) {
+        for(let path2 of list2) {
+            if(path1.some(x => path2.indexOf(x) !== -1)) {
+                continue;
             }
-            else {
-                let allVisited = [...current.visited[0], ...current.visited[1]]
-                for(let i = 0; i < 2; i++) {
-                    let at = current.at[i]
-                    if(current.distance[i] === 0 && !allVisited.includes(at)) {
-                        let valve = valves[at];
-                        let visited = [...current.visited];
-                        if(at !== 'AA') {
-                            visited[i] = [...visited[i], at];
-                            visited[i].sort();
-                        }
-                        let released = current.released + valve.rate * current.time;
-                        let identifier = visited.map(v => v.join(',')).join(':') + '-' + current.at[1 - i];
-                        if(!visited[0].length || !visited[1].length || bestByVisited[identifier] === undefined || bestByVisited[identifier] < released) {
-                            bestByVisited[identifier] = released;
-                            if(released > best) {
-                                console.log('========');
-                                console.log(released);
-                                console.log(current.history);
-                            }
-                            best = Math.max(best, released);
-                            for(let t = 0; t < targets.length; t++) {
-                                let target = targets[t];
-                                if(!allVisited.includes(target)) {
-                                    let skip = false;
-                                    if(current.distance[1 - i] === 0) {
-                                        skip = (t % 2 === i);
-                                    }
-                                    if(!skip) {
-                                        let at = [...current.at];
-                                        at[i] = target;
-                                        let distance = [...current.distance];
-                                        distance[i] = valve.distances[target] + 1;
-                                        let history = [];
-                                        history = [...current.history];
-                                        history[i] = [...history[i], { from: current.at[i], to: target, time: current.time }]
-                                        queue.push({ at, distance, released, visited, time: current.time, history })
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+
+            let amount = findBest(path1) + findBest(path2);
+            best = Math.max(best, amount);
         }
     }
 
@@ -117,7 +125,3 @@ lib.getInput(year, day).then((data) => {
 }).catch((err) => {
     console.log(err, err.stack);
 });
-
-
-// 1986 too low
-// 2219 wrong
