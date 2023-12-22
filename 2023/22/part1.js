0891f76beebfe66ab1ac1392aa33f31c6c14edaa
sguest <spencer.b.guest@gmail.com>
2023-12-22 11:33:48 -0600

    2023-22

---
+++ 2023/22/part1.js
@@ -0,0 +1,111 @@
+let lib = require('../../lib');
+
+let year = 2023;
+let day = 22;
+
+lib.getInput(year, day).then((data) => {
+    let lines = data.split('\n');
+    let bricks = []
+    for(let line of lines) {
+        let [start, end] = line.split('~');
+        let [x, y, z] = start.split(',').map(n => +n);
+        let [endX, endY, endZ] = end.split(',').map(n => +n);
+        let length = (endX - x) + (endY - y) + (endZ - z) + 1;
+        let delta = { x: 0, y: 0, z: 0 };
+        if(endX !== x) {
+            delta.x = 1;
+        }
+        else if(endY !== y) {
+            delta.y = 1;
+        }
+        else {
+            delta.z = 1;
+        }
+
+        bricks.push({ x, y, z, delta, length, stopped: false, supported: [] });
+    }
+
+    let grid = [];
+    let getGrid = (x, y, z) => grid[x] && grid[x][y] && grid[x][y][z];
+    let setGrid = (x, y, z, val) => {
+        grid[x] ||= [];
+        grid[x][y] ||= [];
+        grid[x][y][z] = val;
+    }
+    let getCubes = (brick) => {
+        let cubes = [];
+        for(let i = 0; i < brick.length; i++) {
+            cubes.push({
+                x: brick.x + brick.delta.x * i,
+                y: brick.y + brick.delta.y * i,
+                z: brick.z + brick.delta.z * i,
+            })
+        }
+        return cubes;
+    }
+
+    while(bricks.some(b => !b.stopped)) {
+        for(let brick of bricks.filter(b => !b.stopped)) {
+            brick.z--;
+        }
+
+        let collision = true;
+        while(collision) {
+            collision = false;
+
+            for(let brickId = 0; brickId < bricks.length; brickId++) {
+                let brick = bricks[brickId];
+                if(!brick.stopped) {
+                    if(brick.z <= 0) {
+                        brick.stopped = true;
+                    }
+                    else {
+                        let cubes = getCubes(brick);
+                        for(let cube of cubes) {
+                            let collision = getGrid(cube.x, cube.y, cube.z);
+                            if(collision !== undefined) {
+                                brick.stopped = true;
+                            }
+                        }
+                    }
+
+                    if(brick.stopped) {
+                        collision = true;
+                        brick.z++;
+                        let cubes = getCubes(brick);
+                        for(let cube of cubes) {
+                            setGrid(cube.x, cube.y, cube.z, brickId);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for(let brickId = 0; brickId < bricks.length; brickId++) {
+        let brick = bricks[brickId];
+        let cubes = getCubes(brick);
+        for(let cube of cubes) {
+            let collision = getGrid(cube.x, cube.y, cube.z - 1);
+            if(collision !== undefined && collision !== brickId && brick.supported.indexOf(collision) < 0) {
+                brick.supported.push(collision);
+            }
+        }
+    }
+
+    let count = 0;
+    for(let brickId = 0; brickId < bricks.length; brickId++) {
+        let canRemove = true;
+        for(let brick of bricks) {
+            if(brick.supported.length === 1 && brick.supported[0] === brickId) {
+                canRemove = false;
+            }
+        }
+        if(canRemove) {
+            count++;
+        }
+    }
+    console.log(count);
+}).catch((err) => {
+    console.log(err, err.stack);
+});
